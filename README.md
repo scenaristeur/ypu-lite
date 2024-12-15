03d7412efe118bba28e2343da13dba18735c844e
scenaristeur <scenaristeur@gmail.com>
2024-12-15 15:07:30 +0100

    sync class

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
-# YJSserver
-```HOST=localhost PORT=1234 npx y-websocket
+# connect to good musik
+
+https://www.radiofrance.fr/fip/radio-groove
+
+# YJS provider
+https://docs.yjs.dev/ecosystem/connection-provider/y-websocket
+
+```
+HOST=localhost PORT=1234 npx y-websocket
 running at 'localhost' on port 1234
 
 ```
--- index.js
+++ index.js
@@ -1,69 +1,35 @@
-import * as Y from 'yjs'
-import { WebsocketProvider } from 'y-websocket'
+
 import readline from 'readline'
+import { Sync } from './lib/sync.js'
 
-const ydoc = new Y.Doc()
-// const provider = new WebsocketProvider('wss://ypu.glitch.me/', 'vue-yjs-demo', ydoc)
-const provider = new WebsocketProvider('ws://localhost:1234', 'vue-yjs-demo-messages', ydoc)
-// const ytext = ydoc.getText('shared')
-const yarray = ydoc.getArray('conversation1') 
-
-provider.on('status', event => {
-  console.log('Connection status:', event.status)
-})
-
-// ytext.observe(event => {
-//   console.log('Received update. Current text:', ytext.toString())
-// })
-yarray.observeDeep(yarrayEvent => {
-  yarrayEvent.target === yarray // => true
-
-  // Find out what changed: 
-  // Log the Array-Delta Format to calculate the difference to the last observe-event
-  // console.log("delta", yarrayEvent.changes.delta)
-  // yarray.forEach((message, index) => {
-  //   console.log("message", message.toJSON())
-  // })
-  // console.log("messages",yarray.toJSON())
-
-  logMessages()
-})
-
+let sync_options = {
+  url: 'ws://localhost:1234',
+  room: 'vue-yjs-demo-messages',
+  //debug: true
+}
+let remote_sync_options = {
+  url: 'wss://ypu.glitch.me/',
+  room: 'vue-yjs-demo-messages'
+}
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 })
 
 function promptUser() {
-  rl.question('Enter text to update (or "quit" to exit): ', (input) => {
+  rl.question('Enter text to update (or "quit" to exit): \n', (input) => {
     if (input.toLowerCase() === 'quit') {
       rl.close()
       provider.disconnect()
       process.exit(0)
     } else {
-      const message = new Y.Map()
-      message.set('text', input)
-      message.set('speaker', ydoc.clientID)
-      message.set('role', 'human-cli')
-      // console.log("message", message)
-       ydoc.transact(() => {
-        // yarray.delete(0, yarray.length)
-        yarray.push( [message])
-       })
-      console.log('yarray updated.')
+      sync.addMessage({text: input})
       promptUser()
     }
   })
 }
-function logMessages() {
-  yarray.forEach((message, index) => {
-    console.log("message", message.toJSON())
-  })
-}
 
-provider.on('sync', (isSynced) => {
-  if (isSynced) {
-    console.log('Initial content:', logMessages())
-    promptUser()
-  }
-})
+
+sync_options.promptUser = promptUser
+let sync = new Sync(sync_options)
+
