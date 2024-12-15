98d2e12ae25b0f87144c80174df066b4e2a18044
scenaristeur <scenaristeur@gmail.com>
2024-12-15 15:21:46 +0100

    env

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
-
+import 'dotenv/config'
 import readline from 'readline'
 import { Sync } from './lib/sync.js'
 
 let sync_options = {
-  url: 'ws://localhost:1234',
-  room: 'vue-yjs-demo-messages',
-  //debug: true
-}
-let remote_sync_options = {
-  url: 'wss://ypu.glitch.me/',
-  room: 'vue-yjs-demo-messages'
+  url: process.env.SYNC_URL,
+  room: process.env.SYNC_ROOM,
+  debug: process.env.SYNC_DEBUG
 }
 const rl = readline.createInterface({
   input: process.stdin,
@@ -20,15 +16,18 @@ function promptUser() {
   rl.question('Enter text to update (or "quit" to exit): \n', (input) => {
     if (input.toLowerCase() === 'quit') {
       rl.close()
-      provider.disconnect()
+      sync.provider.disconnect()
       process.exit(0)
     } else {
-      sync.addMessage({text: input})
+      if(input.length>0) {
+        sync.addMessage({text: input})
+      }
+
       promptUser()
     }
   })
 }
-
+console.log(sync_options)
 
 sync_options.promptUser = promptUser
 let sync = new Sync(sync_options)
