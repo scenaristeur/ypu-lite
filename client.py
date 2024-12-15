8c30ed951d01a1054420b1172bce6282c4dc8ac2
scenaristeur <scenaristeur@gmail.com>
2024-12-15 22:59:22 +0100

    python client

--- client.py
+++ client.py
@@ -10,19 +10,22 @@ load_dotenv()  # take environment variables from .env.
 sync_url = os.getenv ("SYNC_URL")
 sync_room = os.getenv ("SYNC_ROOM")
 
-def handle_deep_changes(events: list[ArrayEvent]):
-    # process the events
-    print(events)
-    for event in events:
-        print("-", event.target)
-        print("\ndelta\n", event.delta)
-        print("\npath\n", event.path)
 
 async def client():
     ydoc = Doc()
     # ymap = ydoc.get("map", type=Map)
     yarray = ydoc.get("conversation1", type=Array);
     room_name = sync_room #"my-roomname"
+
+    def handle_deep_changes(events: list[ArrayEvent]):
+    # process the events
+        print(events)
+        for event in events:
+            print("-", event.target)
+            print("\ndelta\n", event.delta)
+            print("\npath\n", event.path)
+        print("to_py\n", yarray.to_py()) 
+
     async with (
         aconnect_ws(f"{sync_url}/{room_name}") as websocket,
         WebsocketProvider(ydoc, HttpxWebsocket(websocket, room_name)),
